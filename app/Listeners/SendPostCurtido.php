f40cd73ce8ce50211c8d6f05aca2e7ceffc68c16
Ryan Guimarães <guimaraesryan26@gmail.com>
2023-11-05 14:25:37 -0300

    fix: implementando interface ShouldQueue

--- app/Listeners/SendPostCurtido.php
+++ app/Listeners/SendPostCurtido.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class SendPostCurtido
+class SendPostCurtido implements ShouldQueue
 {
     /**
      * Create the event listener.
